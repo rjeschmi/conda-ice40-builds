a0544aa1d640ca7f7af73ce575c52d2d3b1e8462
Robert Schmidt <rjeschmi@gmail.com>
2019-08-02 20:37:57 -0400

    add a render for debug

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -4,6 +4,5 @@ git submodule update --init --remote --recursive
 cd conda-hdmi2usb-packages && git checkout osx-add && cd ..
 echo CONDA_PREFIX is "$CONDA_PREFIX"
 conda list
-conda list
 conda config --add channels rjeschmi
 conda config --add channels rjeschmi/label/nightly
